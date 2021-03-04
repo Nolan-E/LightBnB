2031f72ee6db043b4c77d7e6fe9c6d1970919909
Nolan Eckert <nolan.eckert@gmail.com>
2021-03-03 18:03:49 -0700

    Add 01_seeds & directory. Add users seeds.

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (name, email, password)
+VALUES ('Eva Stanley', 'evastanley123@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Louisa Meyer', 'louisa123meyer@examplemail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Dominic Parks', 'dom1n1cparks@wootmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
