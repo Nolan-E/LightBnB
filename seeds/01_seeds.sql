56502453e2d8b2a3fa401b92cf187d880d0382fd
Nolan Eckert <nolan.eckert@gmail.com>
2021-03-03 18:33:29 -0700

    Add reservations seeds.

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -7,3 +7,16 @@ INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover
 VALUES (1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&amp;cs=tinysrgb&amp;h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 930.61, 6, 4, 8, 'Canada', '536 Namsub Highway', 'Sotboske', 'Quebec', '28142', true),
 (2, 'Blank corner', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&amp;cs=tinysrgb&amp;h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 234.71, 6, 4, 8, 'Canada', '123 Nothing Street', 'Hannah', 'Alberta', '23462', true),
 (3, 'Habit mix', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&amp;cs=tinysrgb&amp;h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 897.23, 6, 4, 8, 'Canada', '9870 Blank Circle', 'Simcoe County', 'Ontario', '09876', true);
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+INSERT INTO reservations (guest_id, property_id, start_date, end_date) 
+VALUES (1, 1, '2018-09-11', '2018-09-26'),
+(2, 2, '2019-01-04', '2019-02-01'),
+(3, 3, '2021-10-01', '2021-10-14');
