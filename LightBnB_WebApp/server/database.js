800ec117ca111f829d29e36140471c6c62ac0d85
Nolan Eckert <nolan.eckert@gmail.com>
2021-03-04 21:26:54 -0700

    Update getAllProperties function query to LEFT JOIN to display all created un-rated properties too.

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -92,7 +92,7 @@ const getAllProperties = function(options, limit = 10) {
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) AS average_rating
   FROM properties
-  JOIN property_reviews ON properties.id = property_id
+  LEFT JOIN property_reviews ON properties.id = property_id
   WHERE 1=1
   `;
   if (options.city) {
